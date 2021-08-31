ffed6b8cc01464d459c96fabce49f6653fe799f9
Andrew Bass <andrewbass@Andrews-MacBook-Pro.local>
2021-08-31 00:27:45 -0500

    why

---
+++ content.js
@@ -0,0 +1,31 @@
+// content.js
+// document.getElementById('mat-radio-5-input').checked = true;
+// console.log(document.getElementById("mat-radio-5-input").checked)
+
+setTimeout(function(){ 
+    document.getElementById('mat-radio-5-input').checked = true;
+    console.log(document.getElementById("mat-radio-5-input").checked);
+
+    document.getElementById('mat-checkbox-9-input').checked = true;
+    console.log(document.getElementById("mat-checkbox-9-input").checked);
+
+    document.getElementById('mat-radio-8-input').checked = true;
+    console.log(document.getElementById("mat-radio-8-input").checked);
+
+    document.getElementById('mat-radio-10-input').checked = true;
+    console.log(document.getElementById("mat-radio-10-input").checked);
+
+    document.getElementById('mat-radio-12-input').checked = true;
+    console.log(document.getElementById("mat-radio-12-input").checked);
+
+    document.getElementsByClassName("mat-raised-button").click;
+
+
+
+
+
+}, 3000);
+
+
+
+
